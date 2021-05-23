f1a10f3489638519e71b059b72f495251c2037cf
Arnelle Balane <arnellebalane@gmail.com>
2021-05-23 18:58:09 +0800

    Add remaining LoginCard UI elements

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -1,9 +1,15 @@
 <template>
   <div class="LoginPage">
     <div class="LoginCard">
-      <img src="~_assets/images/logo.png" alt="Instagram logo" />
+      <img class="Logo" src="~_assets/images/logo.png" alt="Instagram logo" />
 
       <LoginForm />
+      <p class="Separator">or</p>
+
+      <button class="LoginButton">
+        <img src="~_assets/images/google-logo.png" alt="Google logo" />
+        Login with Google
+      </button>
     </div>
   </div>
 </template>
@@ -26,15 +32,65 @@ export default {
 }
 
 .LoginCard {
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+
   max-width: 36rem;
   padding: 3.6rem;
   border: 1px solid var(--gray-200);
+  margin-bottom: 20rem;
   background-color: var(--white);
 }
 
-img {
+.Logo {
   display: block;
-  width: 60%;
+  width: 65%;
   margin: 0 auto;
+  margin-bottom: 1.6rem;
+}
+
+.Separator {
+  position: relative;
+  font-size: 1.4rem;
+  font-weight: 700;
+  text-align: center;
+  text-transform: uppercase;
+  color: var(--gray-400);
+}
+
+.Separator::before,
+.Separator::after {
+  content: '';
+  position: absolute;
+  top: 50%;
+  width: 40%;
+  border-top: 1px solid var(--gray-300);
+}
+
+.Separator::before {
+  left: 0;
+}
+
+.Separator::after {
+  right: 0;
+}
+
+.LoginButton {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 8px;
+
+  border: none;
+  font-size: 1.4rem;
+  font-weight: 700;
+  color: var(--gray-700);
+  background: none;
+  cursor: pointer;
+}
+
+.LoginButton img {
+  width: 1.4rem;
 }
 </style>
