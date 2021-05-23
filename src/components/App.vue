a40f76818ee1c14a81a6ad27e276cc1feb5545f2
Arnelle Balane <arnellebalane@gmail.com>
2021-05-23 18:36:51 +0800

    Add design for LoginForm component

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,3 +1,30 @@
 <template>
-  <h1>Instagram</h1>
+  <div class="Wrapper">
+    <LoginPage class="Page" />
+  </div>
 </template>
+
+<script>
+import LoginPage from '_pages/LoginPage.vue';
+
+export default {
+  components: {
+    LoginPage,
+  },
+};
+</script>
+
+<style scoped>
+.Wrapper {
+  display: flex;
+  flex-direction: column;
+
+  max-width: 50rem;
+  min-height: 100vh;
+  margin: 0 auto;
+}
+
+.Page {
+  flex-grow: 1;
+}
+</style>
---
+++ src/pages/LoginPage.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="LoginPage">
+    <div class="LoginCard">
+      <img src="~_assets/images/logo.png" alt="Instagram logo" />
+
+      <LoginForm />
+    </div>
+  </div>
+</template>
+
+<script>
+import LoginForm from '_components/LoginForm.vue';
+
+export default {
+  components: {
+    LoginForm,
+  },
+};
+</script>
+
+<style scoped>
+.LoginPage {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.LoginCard {
+  max-width: 36rem;
+  padding: 3.6rem;
+  border: 1px solid var(--gray-200);
+  background-color: var(--white);
+}
+
+img {
+  display: block;
+  width: 60%;
+  margin: 0 auto;
+}
+</style>
