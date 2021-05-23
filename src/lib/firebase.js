b10d85f7dcf92c88fa58f3444bf265a920d28072
Arnelle Balane <arnellebalane@gmail.com>
2021-05-23 20:52:55 +0800

    Initialize Firebase application

---
+++ src/lib/firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+firebase.initializeApp({
+  apiKey: 'AIzaSyAXdcZ5iPaIVvQigRaJLirDtyxehC2sMkw',
+  authDomain: 'instagram-clone-2d8bc.firebaseapp.com',
+  projectId: 'instagram-clone-2d8bc',
+  storageBucket: 'instagram-clone-2d8bc.appspot.com',
+  messagingSenderId: '595004279383',
+  appId: '1:595004279383:web:c8dec3d037e90c68677112',
+});
+
+export default firebase;
