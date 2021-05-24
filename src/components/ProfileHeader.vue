bd48d5ee0942383f961ac05a96088e57445e4fb9
Arnelle Balane <arnellebalane@gmail.com>
2021-05-25 02:55:55 +0800

    Add ProfileHeader component

---
+++ src/components/ProfileHeader.vue
@@ -0,0 +1,47 @@
+<template>
+  <header class="ProfileHeader">
+    <img :src="user.photoURL" :alt="user.name" />
+
+    <div>
+      <h1>{{ user.name }}</h1>
+      <p>{{ user.description }} lorem</p>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  props: {
+    user: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  gap: 3.6rem;
+}
+
+img {
+  width: 8rem;
+  height: 8rem;
+  object-fit: cover;
+  object-position: center center;
+  border: 1px solid var(--gray-300);
+  border-radius: 50%;
+}
+
+div {
+  padding-top: 1.2rem;
+}
+
+h1 {
+  margin-bottom: 4px;
+  font-size: 2.4rem;
+  font-weight: 400;
+}
+</style>
--- src/pages/ProfilePage.vue
+++ src/pages/ProfilePage.vue
@@ -1,3 +1,30 @@
 <template>
-  <h1>ProfilePage</h1>
+  <div class="ProfilePage">
+    <ProfileHeader :user="user" />
+  </div>
 </template>
+
+<script>
+import ProfileHeader from '@components/ProfileHeader.vue';
+
+export default {
+  components: {
+    ProfileHeader,
+  },
+
+  data() {
+    return {
+      user: {
+        id: 'abc123',
+        name: 'Arnelle Balane',
+        email: 'arnellebalane@gmail.com',
+        photoURL: 'https://lh3.googleusercontent.com/a-/AOh14GgvGHJODi4nZbu_nj5UCVirg3aR0jpzkHgJvAr2og=s96-c',
+        postsCount: 10,
+        followersCount: 100,
+        followingCount: 20,
+        description: 'Software Developer, Newlogic',
+      },
+    };
+  },
+};
+</script>
