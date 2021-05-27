1b6aae6cf66a9a62e4e7db086cbc6cfcabc317ed
Arnelle Balane <arnellebalane@gmail.com>
2021-05-28 04:15:40 +0800

    Add script to create user profiles for existing users

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,6 @@
 const path = require('path');
 require('dotenv').config({
-  path: path.resolve(__dirname, '.env.local'),
+  path: path.resolve(__dirname, '../.env.local'),
 });
 
 const admin = require('firebase-admin');
