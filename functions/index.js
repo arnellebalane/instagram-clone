2456715543ec0d485dffba5f038a93cf3f24a0ee
Arnelle Balane <arnellebalane@gmail.com>
2021-05-27 14:48:56 +0800

    Add cloud function to update post's latest comments

--- functions/index.js
+++ functions/index.js
@@ -3,12 +3,28 @@ require('dotenv').config({
   path: path.resolve(__dirname, '.env.local'),
 });
 
+const admin = require('firebase-admin');
 const functions = require('firebase-functions');
 
-exports.amendNewPostData = functions.firestore.document('posts/{doc}').onCreate((change) => {
+admin.initializeApp();
+const db = admin.firestore();
+
+exports.amendNewPostData = functions.firestore.document('posts/{post}').onCreate((change) => {
   return change.ref.update({
     likesCount: 0,
     commentsCount: 0,
     latestComments: [],
+  });
+});
+
+exports.updatePostCommentsData = functions.firestore.document('posts/{post}/comments/{comment}').onCreate((change) => {
+  return db.runTransaction(async (t) => {
+    const postRef = change.ref.parent.parent;
+    const post = await t.get(postRef);
+    const comment = await t.get(change.ref);
+    return t.update(postRef, {
+      commentsCount: post.data().commentsCount + 1,
+      latestComments: post.data().latestComments.concat(comment.data()).slice(-2),
+    });
   });
 });
