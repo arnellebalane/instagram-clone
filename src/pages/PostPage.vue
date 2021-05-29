5ea884e25b15755bbeef02be188973b623b81b3a
Arnelle Balane <arnellebalane@gmail.com>
2021-05-29 09:55:52 +0800

    Display PagePage from Firestore

--- src/pages/PostPage.vue
+++ src/pages/PostPage.vue
@@ -1,5 +1,49 @@
 <template>
   <div class="PostPage">
-    <h1>PostPage</h1>
+    <Post v-if="post" :post="post" />
   </div>
 </template>
+
+<script>
+import { db } from '@lib/firebase';
+import Post from '@components/Post.vue';
+
+export default {
+  components: {
+    Post,
+  },
+
+  data() {
+    return {
+      post: null,
+      comments: [],
+    };
+  },
+
+  mounted() {
+    const postId = this.$route.params.id;
+
+    this.unsubscribePost = db.doc(`posts/${postId}`).onSnapshot((doc) => {
+      if (doc.exists) {
+        this.post = { ...doc.data(), id: doc.id };
+      } else {
+        this.$router.push({ name: 'not-found' });
+      }
+    });
+
+    this.unsubscribeComments = db
+      .collection(`posts/${postId}/comments`)
+      .orderBy('datePosted', 'asc')
+      .onSnapshot((snapshot) => {
+        this.comments = snapshot.docs
+          .filter((doc) => !doc.metadata.hasPendingWrites)
+          .map((doc) => ({ ...doc.data(), id: doc.id }));
+      });
+  },
+
+  unmounted() {
+    this.unsubscribePost();
+    this.unsubscribeComments();
+  },
+};
+</script>
